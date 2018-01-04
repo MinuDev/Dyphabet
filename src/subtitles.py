6f9c16f922cc37f992584d6870a692ec893dd929
Agustín Roth <agusrothdev@gmail.com>
2018-01-03 21:46:56 -0300

    Started the subtitles module

---
+++ src/subtitles.py
@@ -0,0 +1,9 @@
+# This class represents the whole subtitle file
+class File:
+    # This is a list that will later contain all the sections of the file
+    sections = []
+
+    sections.append(Section())
+
+# This class represents a section in the file. Each section has a timestamp, text and index.
+class Section:
