aa44fe5b554794f274ff2a02e3f20260bf1bcedd
Facundo E. Mendoza <facundomendoza2405@gmail.com>
2018-01-07 11:02:54 -0300

    Trying to make a menu

--- src/__main__.py
+++ src/__main__.py
@@ -1,5 +1,9 @@
-import gui
+from gui import GUI
+import sys
+from PyQt5.QtWidgets import QApplication
 
 if __name__ == "__main__":
     # TODO: Call the GUI module to start the main Window
-    UserInterface = GUI
+    UI = QApplication(sys.argv)
+    userInterface = GUI()
+    sys.exit(app.exec_())
--- src/gui.py
+++ src/gui.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QWidget, QAction, QMenu
 
 class GUI(QWidget):
 
@@ -8,6 +8,28 @@ class GUI(QWidget):
         self.initUI()
 
     def initUI(self):
+        """menubar = self.menuBar()
+        fileMenu = menubar.addMenu('File')
+
+        openFileMenu = QAction('Open file...', self)
+        fileMenu.addAction(openFileMenu)
+
         self.setGeometry(300, 300, 300, 220)
         self.setWindowTitle('Dyphabet')
+        self.show()"""
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('File')
+
+        impMenu = QMenu('Import', self)
+        impAct = QAction('Import mail', self)
+        impMenu.addAction(impAct)
+
+        newAct = QAction('New', self)
+
+        fileMenu.addAction(newAct)
+        fileMenu.addMenu(impMenu)
+
+        self.setGeometry(300, 300, 300, 200)
+        self.setWindowTitle('Submenu')
         self.show()
